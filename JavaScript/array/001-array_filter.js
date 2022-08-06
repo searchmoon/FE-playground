dff19b049192dbd5e59d40341d8bacd54c5d0a95
searchmoon <moonding901212@gmail.com>
2022-08-06 10:20:33 +0900

    소스수정

--- JavaScript/array/001-array_filter.js
+++ JavaScript/array/001-array_filter.js
@@ -1,27 +1,23 @@
-const sampleData = {
-    rnpRate: [
-        { coin: 'RNP', rate: 1 },
-        { coin: 'ETH', rate: 0.000061 },
-        { coin: 'BNB', rate: 0.000317 }
+const data = {
+    exchangeRate: [
+        { cash: 'USD', rate: 1 },
+        { cash: 'KRW', rate: 1304 },
+        { cash: 'JPY', rate: 966.03 }
     ],
 };
 
 console.log('---- 01. 전통적인 for 문 ------')
-for(let i = 0; i < sampleData.rnpRate.length; i++) {
-    const obj = sampleData.rnpRate[i];
+for(let i = 0; i < data.exchangeRate.length; i++) {
+    const obj = data.exchangeRate[i];
 
-    if(sampleData.rnpRate[i].coin === 'BNB'){
-        console.log('bnb', obj);
+    if(data.exchangeRate[i].cash === 'KRW'){
+        console.log('KRW', obj);
         console.log('rate : ', obj.rate);
     }
 }
 console.log('--- 02. 배열 filter로 검색 ---- 필터에서는 배열 리턴하는데 값이 없으면 빈 배열')
-const result = sampleData.rnpRate.filter(item => item.coin === 'BNB');
+const result = data.exchangeRate.filter(item => item.cash === 'KRW');
 console.log('result : ', result);
-console.log('?hello world');
 
-// 01. sampleData.rnpRate 배열 아이템을 각자 순회하면서 콘솔 로그 출력
-
-// 02. 아이템들 각자 순회하면서 -> 콘솔로그도 하면서 coin 이 BNB 인 것 if 문으로 찾기
 
 
