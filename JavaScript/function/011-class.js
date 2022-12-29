fc4d72c691bf93dcd0588c805b3db7bdef4a1fe3
Rumi <moon901212@gmail.com>
2022-12-29 22:28:07 +0900

    class

--- JavaScript/function/011-class.js
+++ JavaScript/function/011-class.js
@@ -38,6 +38,8 @@ console.log(user); // User { name: '루미', hi: 'hi' }
 // class 를 생성자 함수로 호출하면
 // 1.새로운 객체가 생성된다.
 // 2.넘겨받은 인수와 함께 constructor가 자동으로 실행된다. 이때 인수 '루미'가 this.name에 할당된다.
+// constructor(생성자)를 이용하면 class 객체의 초기 값을 설정해 줄 수 있다.
+
 
 // 그래서, class가 뭔데요?
 // 1. 위에서 예를 든 class User {...} 는 User라는 이름을 가진 함수를 만든다. 함수 본문은 생성자 메서드 constructor에서 가져온다.
@@ -54,4 +56,36 @@ let User2 = class MyClass {
 new User2().sayHi(); // MyClass { sayHi() { console.log(MyClass); } }
 //console.log(MyClass); // ReferenceError: MyClass is not defined
 
+
+// ----------
+
+class Developer {
+  //타입스크립트 인터페이스
+  // name: string;
+  // age: number;
+  constructor (name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat(restaurant){
+    console.log(`${this.name}은 ${restaurant}에서 밥을 먹는다`);
+  }
+}
+const result = new Developer('루미', 20);
+console.log(result.eat('맥도날드'));
+
+
+// class 문법을 기본 function으로 바꾸기
+
+function developer(name, age) {
+  this.name = name;
+  this.age = age;
+
+  developer.prototype.eat = function(restaurant) {
+    console.log(`${this.name}는 ${restaurant}에서 밥을 먹는다`);
+  };
+}
+//호출하기
+const result2 = new developer('루미', 20);
+console.log(result2.eat('롯데리아'));
 
