873839b5e69673f5b2fe539fa0b51eed532e3d9b
최강용 <arahansa@naver.com>
2022-12-30 17:30:40 +0900

    add git ignore

--- typescript/src/helloworld/helloworld.ts
+++ typescript/src/helloworld/helloworld.ts
@@ -1,20 +1,13 @@
-
-
-
-
 console.log('hello world')
 
 interface 숨쉬기{
     숨쉰다: () => void
 }
 
-
 class 사람 implements 숨쉬기{
     숨쉰다(): void {
         console.log('전 입으로 숨을 쉽니다')
     }
-
 }
-
 
 (new 사람).숨쉰다()
