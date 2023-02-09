c14388afe4e5399e9bde65c416074472f0c19314
Rumi <moon901212@gmail.com>
2023-02-09 21:20:28 +0900

    09. 타입 변환과 단축평가 완료

---
+++ JavaScript/ModernJs-DeepDive/09.타입변환.js
@@ -0,0 +1,193 @@
+
+//09- 타입 변환과 단축 평가
+
+// <1. 타입변환이란? >
+//09-03
+// 피연산자가 모두 문자열 타입이어야 하는 문맥
+'10' + 2 // -> '102'
+
+// 피연산자가 모두 숫자 타입이어야 하는 문맥
+5 * '10' // -> 50
+
+// 피연산자 또는 표현식이 불리언 타입이어야 하는 문맥
+!0 // -> true
+if (1) { }
+
+// falsy 한 data: null, 0, undefined, NaN, '', false
+
+// <2. 암묵적 타입 변환>
+
+// 09-06
+// 숫자 타입
+0 + ''         // -> "0"
+-0 + ''        // -> "0"
+1 + ''         // -> "1"
+-1 + ''        // -> "-1"
+NaN + ''       // -> "NaN"
+Infinity + ''  // -> "Infinity"
+-Infinity + '' // -> "-Infinity"
+
+// 불리언 타입
+true + ''  // -> "true"
+false + '' // -> "false"
+
+// null 타입
+null + '' // -> "null"
+
+// undefined 타입
+undefined + '' // -> "undefined"
+
+  // 심벌 타입
+  (Symbol()) + '' // -> TypeError: Cannot convert a Symbol value to a string
+
+  // 객체 타입
+  ({}) + ''           // -> "[object Object]"
+Math + ''           // -> "[object Math]"
+  [] + ''             // -> ""
+  [10, 20] + ''       // -> "10,20"
+  (function(){}) + '' // -> "function(){}"
+Array + ''          // -> "function Array() { [native code] }"
+
+
+// 09-09
+// 문자열 타입
++''       // -> 0
++'0'      // -> 0
++'1'      // -> 1
++'string' // -> NaN
+
+// 불리언 타입
++true     // -> 1
++false    // -> 0
+
+// null 타입
++null     // -> 0
+
+// undefined 타입
++undefined // -> NaN
+
+// 심벌 타입
++Symbol() // -> TypeError: Cannot convert a Symbol value to a number
+
+// 객체 타입
++{}             // -> NaN   객체 앞에 붙이면 NaN 인데
++[]             // -> 0     배열 앞에 붙이면 0 이네! 기억하기.
++[10, 20]       // -> NaN
++(function(){}) // -> NaN
+
+
+// 09-14
+// 1. String 생성자 함수를 new 연산자 없이 호출하는 방법
+// 숫자 타입 => 문자열 타입
+String(1);        // -> "1"
+String(NaN);      // -> "NaN"
+String(Infinity); // -> "Infinity"
+// 불리언 타입 => 문자열 타입
+String(true);     // -> "true"
+String(false);    // -> "false"
+
+// 2. Object.prototype.toString 메서드를 사용하는 방법
+// 숫자 타입 => 문자열 타입
+(1).toString();        // -> "1"
+(NaN).toString();      // -> "NaN"
+(Infinity).toString(); // -> "Infinity"
+// 불리언 타입 => 문자열 타입
+(true).toString();     // -> "true"
+(false).toString();    // -> "false"
+
+// 3. 문자열 연결 연산자를 이용하는 방법
+// 숫자 타입 => 문자열 타입
+1 + '';        // -> "1"
+NaN + '';      // -> "NaN"
+Infinity + ''; // -> "Infinity"
+// 불리언 타입 => 문자열 타입
+true + '';     // -> "true"
+false + '';    // -> "false"
+
+// 09-15
+// 1. Number 생성자 함수를 new 연산자 없이 호출하는 방법
+// 문자열 타입 => 숫자 타입
+Number('0');     // -> 0
+Number('-1');    // -> -1
+Number('10.53'); // -> 10.53
+// 불리언 타입 => 숫자 타입
+Number(true);    // -> 1
+Number(false);   // -> 0
+
+// 2. parseInt, parseFloat 함수를 사용하는 방법(문자열만 변환 가능)
+// 문자열 타입 => 숫자 타입
+parseInt('0');       // -> 0
+parseInt('-1');      // -> -1
+parseFloat('10.53'); // -> 10.53
+// parseInt( '123', 16) 이렇게 하면, 16진법으로 변환된다. // 291
+
+// 3. + 단항 산술 연산자를 이용하는 방법
+// 문자열 타입 => 숫자 타입
++'0';     // -> 0
++'-1';    // -> -1
++'10.53'; // -> 10.53
+// 불리언 타입 => 숫자 타입
++true;    // -> 1
++false;   // -> 0
+
+// 4. * 산술 연산자를 이용하는 방법
+// 문자열 타입 => 숫자 타입
+'0' * 1;     // -> 0
+'-1' * 1;    // -> -1
+'10.53' * 1; // -> 10.53
+// 불리언 타입 => 숫자 타입
+true * 1;    // -> 1
+false * 1;   // -> 0
+
+// <4. 단축 평가>
+
+// 09-19
+// 논리합(||) 연산자
+'Cat' || 'Dog'  // -> "Cat"
+false || 'Dog'  // -> "Dog"
+'Cat' || false  // -> "Cat"
+// 논리합(||) 연산자는 처음 만난 truthy 값 또는 다음번의 truthy 값 또는 마지막 falsy 값 반환
+
+// 논리곱(&&) 연산자
+'Cat' && 'Dog'  // -> "Dog"
+false && 'Dog'  // -> false
+'Cat' && false  // -> false
+// 논리곱(&&) 연산자는 마지막 truthy값 또는 처음 만난 falsy값 반환
+
+
+// 표현을 달리할 수 있다. 예를 들어
+let done = false;
+message = done && '완료' || '미완료'; //'미완료'
+// 이거랑 삼항연산자인
+done ? '완료' : '미완료' // '미완료'
+// 는 같은 표현이다.
+
+// 09-26
+// 옵셔널 체이닝 연산자
+
+let elem = null;
+
+// elem이 null 또는 undefined이면 undefined를 반환하고, 그렇지 않으면 우항의 프로퍼티 참조를 이어간다.
+let value = elem?.value;
+console.log(value); // undefined
+
+//09-30
+// null 병합 연산자
+
+// 좌항의 피연산자가 null 또는 undefined이면 우항의 피연산자를 반환하고, 그렇지 않으면 좌항의 피연산자를 반환한다.
+var foo = null ?? 'default string';
+console.log(foo); // "default string"
+
+//09-31
+// Falsy 값인 0이나 ''도 기본값으로서 유효하다면 예기치 않은 동작이 발생할 수 있다.
+var foo = '' || 'default string';
+console.log(foo); // "default string"
+
+//09-32
+// 좌항의 피연산자가 Falsy 값이라도 null 또는 undefined가 아니면 좌항의 피연산자를 반환한다.
+var foo = '' ?? 'default string';
+console.log(foo); // ""
+
+
+
+
