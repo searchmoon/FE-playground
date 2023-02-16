cbc9a6c756a12289f4a27df75c191eda3de6d441
Rumi <moon901212@gmail.com>
2023-02-16 22:55:15 +0900

    10. 객체리터럴 완료

---
+++ JavaScript/ModernJs-DeepDive/10.객체리터럴.js
@@ -0,0 +1,221 @@
+
+// 10. 객체 리터럴
+
+// 객체란? 자바스크립트는 객체 기반의 프로그래밍 언어이며, 자바스크립트를 구성하는 거의 '모든 것' 이 객체다.
+// 원시 값을 제외한 나머지 값(함수, 배열, 정규 표현식 등)은 모두 객체이다.
+
+// 인스턴스란? 클래스에 의해 생성되어 메모리에 저장된 실체를 말한다. 객체지향 프로그래밍에서 객체는 클래스와 인스턴스를 포함한 개념이다.
+// 클래스는 인스턴스를 생성하기 위한 템플릿의 역할을 한다. 인스턴스는 객체가 메모리에 저장되어 실제로 존재하는 것에 초점을 맞춘 용어이다.
+
+//10-01
+let person = {
+  name: 'Lee',
+  sayHello: function () {
+    console.log(`Hello! My name is ${this.name}.`);
+  }
+};
+
+console.log(typeof person); // object
+console.log(person); // {name: "Lee", sayHello: ƒ}
+console.log(person.sayHello()); //Hello! My name is Lee. (객체 안의 메소드 실행)
+
+//10-04
+let person = {
+  firstName: 'Ung-mo', // 식별자 네이밍 규칙을 준수하는 프로퍼티 키
+  'last-name': 'Lee'   // 식별자 네이밍 규칙을 준수하지 않는 프로퍼티 키
+};
+console.log(person); // {firstName: "Ung-mo", last-name: "Lee"}
+
+// 10-05
+let person = {
+  firstName: 'Ung-mo',
+  last-name: 'Lee' // SyntaxError: Unexpected token -
+};
+
+//10-06 ***
+let obj = {};
+let key = 'hello';
+
+// ES5: 프로퍼티 키 동적 생성
+obj[key] = 'world';
+// ES6: 계산된 프로퍼티 이름
+// let obj = { [key]: 'world' };
+
+console.log(obj); // {hello: "world"}
+
+//10-08
+// 프로퍼티 키에 문자열이나 심벌 값 외의 값을 사용하면 암묵적 타입 변환을 통해 문자열이 된다.
+
+let foo = {
+  0: 1,
+  1: 2,
+  2: 3
+};
+
+console.log(foo); // {0: 1, 1: 2, 2: 3}
+
+//10-10
+//동일한 프로퍼티 키가 있으면 마지막에 선언된 키: 값이 적용이된다.
+let foo = {
+  name: 'Lee',
+  name: 'Kim'
+};
+
+console.log(foo); // {name: "Kim"}
+
+//10-11
+let circle = {
+  radius: 5, // ← 프로퍼티
+
+  // 원의 지름
+  getDiameter: function () { // ← 메서드
+    return 2 * this.radius; // this는 circle을 가리킨다.
+  }
+};
+
+console.log(circle.getDiameter()); // 10
+
+//<5. 프로퍼티의 접근>
+
+//10-12
+let person = {
+  name: 'Lee'
+};
+
+// 마침표 표기법에 의한 프로퍼티 접근
+console.log(person.name); // Lee
+
+// 대괄호 표기법에 의한 프로퍼티 접근
+console.log(person['name']); // Lee
+
+//10-13
+let person = {
+  name: 'Lee'
+};
+
+console.log(person[name]); // ReferenceError: name is not defined
+
+//10-14
+let person = {
+  name: 'Lee'
+};
+
+console.log(person.age); // undefined
+
+//10-15
+let person = {
+  'last-name': 'Lee',
+  1: 10
+};
+
+person.'last-name';  // -> SyntaxError: Unexpected string
+person.last-name;    // -> 브라우저 환경: NaN
+                     // -> Node.js 환경: ReferenceError: name is not defined
+person[last-name];   // -> ReferenceError: last is not defined
+person['last-name']; // -> Lee
+
+// 프로퍼티 키가 숫자로 이뤄진 문자열인 경우 따옴표를 생략할 수 있다.
+person.1;     // -> SyntaxError: Unexpected number
+person.'1';   // -> SyntaxError: Unexpected string
+person[1];    // -> 10 : person[1] -> person['1']
+person['1'];  // -> 10
+
+//10-16
+let person = {
+  name: 'Lee'
+};
+
+// person 객체에 name 프로퍼티가 존재하므로 name 프로퍼티의 값이 갱신된다.
+person.name = 'Kim';
+
+console.log(person);  // {name: "Kim"}
+
+//10-17
+let person = {
+  name: 'Lee'
+};
+
+// person 객체에는 age 프로퍼티가 존재하지 않는다.
+// 따라서 person 객체에 age 프로퍼티가 동적으로 생성되고 값이 할당된다.
+person.age = 20;
+
+console.log(person); // {name: "Lee", age: 20}
+
+//10-18 프로퍼티의 삭제
+let person = {
+  name: 'Lee'
+};
+
+// 프로퍼티 동적 생성
+person.age = 20;
+
+// person 객체에 age 프로퍼티가 존재한다.
+// 따라서 delete 연산자로 age 프로퍼티를 삭제할 수 있다.
+delete person.age;
+
+Object.defineProperty(person, 'age', { value: 20, configurable: false })
+// 이렇게 하면 delete person.age 해도 age가 삭제되지 않는다.
+// person 객체에 address 프로퍼티가 존재하지 않는다.
+// 따라서 delete 연산자로 address 프로퍼티를 삭제할 수 없다. 이때 에러가 발생하지 않는다.
+delete person.address;
+
+console.log(person); // {name: "Lee"}
+
+// < 9. ES6에서 추가된 객체 리터럴의 확장 기능>
+//10-19 프로퍼티 축약 표현
+var x = 1, y = 2;
+// ES5
+var obj = {
+  x: x,
+  y: y
+};
+//ES6 부터는 이렇게 표현도 가능해졌다.
+var obj = {
+  x,
+  y
+};
+
+console.log(obj); // {x: 1, y: 2}
+
+// 10-21
+var prefix = 'prop';
+var i = 0;
+var obj = {};
+
+// ES5
+// 계산된 프로퍼티 이름으로 프로퍼티 키 동적 생성
+obj[prefix + '-' + ++i] = i;
+obj[prefix + '-' + ++i] = i;
+obj[prefix + '-' + ++i] = i;
+
+// ES6
+// 객체 리터럴 내부에서 계산된 프로퍼티 이름으로 프로퍼티 키 동적 생성
+const obj = {
+  [`${prefix}-${++i}`]: i,
+  [`${prefix}-${++i}`]: i,
+  [`${prefix}-${++i}`]: i
+};
+
+console.log(obj); // {prop-1: 1, prop-2: 2, prop-3: 3}
+
+//10-23
+// ES5
+var obj = {
+  name: 'Lee',
+  sayHi: function() {
+    console.log('Hi! ' + this.name);
+  }
+};
+
+obj.sayHi(); // Hi! Lee
+//10-24
+// ES6
+const obj = {
+  name: 'Lee',
+  // 메서드 축약 표현
+  sayHi() {
+    console.log('Hi! ' + this.name);
+  }
+};
+
+obj.sayHi(); // Hi! Lee
