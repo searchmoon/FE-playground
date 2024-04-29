635e2d427e2f3df5c5cc847f23ba5c0236d4c4d2
searchmoon <moon901212@gmail.com>
2024-04-29 23:46:55 +0900

    0단계 풀이

--- JavaScript/programmers/level-0/11-20.js
+++ JavaScript/programmers/level-0/11-20.js
@@ -1,12 +1,51 @@
 //11. 문자열 겹쳐쓰기 https://school.programmers.co.kr/learn/courses/30/lessons/181943
 
-//11. 풀이
+//11. 풀이 1.
 function solution(my_string, overwrite_string, s) {
-    let strArr = my_string.split('');
-    
-    for (let i = 0; i < overwrite_string.length; i++) {
-        strArr[s + i] = overwrite_string[i];
-    }
-    
-    return strArr.join('');
+  let strArr = my_string.split("");
+
+  for (let i = 0; i < overwrite_string.length; i++) {
+    strArr[s + i] = overwrite_string[i];
+  }
+
+  return strArr.join("");
 }
+
+//11. 풀이 2.
+function solution(my_string, overwrite_string, s) {
+  return (
+    my_string.substring(0, s) +
+    overwrite_string +
+    my_string.substring(s + overwrite_string.length)
+  );
+}
+
+//12. 할 일 목록 https://school.programmers.co.kr/learn/courses/30/lessons/181885
+
+//12. 풀이 1.
+function solution(todo_list, finished) {
+  let result = [];
+
+  for (let i = 0; i < todo_list.length; i++) {
+    if (!finished[i]) {
+      result.push(todo_list[i]);
+    }
+  }
+
+  return result;
+}
+
+//12. 풀이 2.
+function solution(todo_list, finished) {
+  let answer = [];
+  return todo_list.filter((e, i) => !finished[i]);
+}
+
+//13. 문자열 정렬하기 (2) https://school.programmers.co.kr/learn/courses/30/lessons/120911
+
+//13. 풀이 1.
+function solution(my_string) {
+    return my_string.toLowerCase().split('').sort().join('');
+}
+
+
