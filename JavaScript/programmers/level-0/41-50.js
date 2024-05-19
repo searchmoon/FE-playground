942868ad6284f35db2911d299b7c0f1c936c8776
searchmoon <moon901212@gmail.com>
2024-05-19 23:57:00 +0900

    0단계 풀이

--- JavaScript/programmers/level-0/41-50.js
+++ JavaScript/programmers/level-0/41-50.js
@@ -17,3 +17,58 @@ function solution(arr) {
 
   return arr.slice(firstIdx, lastIdx + 1);
 }
+
+//43. 날짜 비교하기 https://school.programmers.co.kr/learn/courses/30/lessons/181838#
+function solution(date1, date2) {
+  for (let i = 0; i < 3; i++) {
+    if (date1[i] < date2[i]) {
+      return 1;
+    } else if (date1[i] > date2[i]) {
+      return 0;
+    }
+  }
+  return 0;
+}
+
+//44. 간단한 논리 연산 https://school.programmers.co.kr/learn/courses/30/lessons/181917
+function solution(x1, x2, x3, x4) {
+  let bool1 = x1 || x2;
+  let bool2 = x3 || x4;
+
+  return bool1 && bool2;
+}
+
+//45. 주사위 게임 2 https://school.programmers.co.kr/learn/courses/30/lessons/181930
+// 풀이 1.
+function solution(a, b, c) {
+  const pow1 = a + b + c;
+  const pow2 = Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2);
+  const pow3 = Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3);
+  if (a === b && b === c) {
+    return pow1 * pow2 * pow3;
+  } else if (a === b || b === c || a === c) {
+    return pow1 * pow2;
+  } else {
+    return pow1;
+  }
+}
+
+//풀이 2.
+const solution = (a, b, c) => {
+  if (a === b && b === c) {
+    return calculate([a, b, c], 3);
+  } else if (a === b || b === c || a === c) {
+    return calculate([a, b, c], 2);
+  } else {
+    return calculate([a, b, c]);
+  }
+};
+
+const calculate = (inc, n = 1) => {
+  const [a, b, c] = inc;
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i);
+  }
+  return result;
+};
