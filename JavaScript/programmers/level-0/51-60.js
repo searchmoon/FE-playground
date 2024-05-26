6e140304a94071d1e5b7dfe5405f947ed5c4cf34
searchmoon <moon901212@gmail.com>
2024-05-26 23:48:54 +0900

    0단계 풀이

--- JavaScript/programmers/level-0/51-60.js
+++ JavaScript/programmers/level-0/51-60.js
@@ -59,23 +59,70 @@ function solution(n) {
 //53. 접미사 배열 https://school.programmers.co.kr/learn/courses/30/lessons/181909
 
 function solution(my_string) {
-    let answer = [];
-    for(let i = 0; i < my_string.length; i++){
-        answer.push(my_string.slice(i));
-    }
-    return answer.sort();
+  let answer = [];
+  for (let i = 0; i < my_string.length; i++) {
+    answer.push(my_string.slice(i));
+  }
+  return answer.sort();
 }
 
 //54. 문자 개수 세기 https://school.programmers.co.kr/learn/courses/30/lessons/181902
 
 function solution(my_string) {
-    let arr = Array(52).fill(0);
-    const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let arr = Array(52).fill(0);
+  const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    for(let character of my_string){
-        let index = str.indexOf(character);
-        arr[index]++;
+  for (let character of my_string) {
+    let index = str.indexOf(character);
+    arr[index]++;
+  }
+
+  return arr;
+}
+
+//55. 문자열 계산하기 https://school.programmers.co.kr/learn/courses/30/lessons/120902#
+
+function solution(my_string) {
+  const arr = my_string.split(" ");
+  let result = Number(arr[0]);
+
+  for (let i = 1; i < arr.length; i += 2) {
+    const operator = arr[i];
+    const nextNumber = Number(arr[i + 1]);
+
+    if (operator === "+") {
+      result += nextNumber;
+    } else if (operator === "-") {
+      result -= nextNumber;
     }
+  }
 
-    return arr;
+  return result;
+}
+
+//56. 커피 심부름 https://school.programmers.co.kr/learn/courses/30/lessons/181837
+function solution(order) {
+  let answer = 0;
+
+  for (let pick of order) {
+    if (pick.includes("cafelatte")) {
+      answer += 5000;
+    } else {
+      answer += 4500;
+    }
+  }
+
+  return answer;
+}
+
+//57. qr code https://school.programmers.co.kr/learn/courses/30/lessons/181903
+function solution(q, r, code) {
+  let answer = "";
+  let arr = code.split("");
+
+  for (let i = r; i < code.length; i += q) {
+    answer += arr[i];
+  }
+
+  return answer;
 }
